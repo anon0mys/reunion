a3db32d010a445d886f35259445752ca861d56f9
Evan Wheeler <anon0mys87@gmail.com>
2018-02-21 10:55:26 -0700

    First attempt at ERB

---
+++ runner.rb
@@ -0,0 +1,30 @@
+require 'erb'
+require './lib/reunion'
+
+# Runs reunion and builds erb html page
+class ReunionRunner
+  def initialize
+    @reunion = Reunion.new
+    add_reunion_events
+    erb_render
+  end
+
+  def add_reunion_events
+    activities = { 'Boat Ride' => { 'Jim' => 10.00,
+                                    'Bob' => 35.00 },
+                   'Cabana' => { 'Jim' => 25.00,
+                                 'Bob' => 5.00 } }
+    activities.each do |name, participants|
+      @reunion.add_activity(name, participants)
+    end
+  end
+
+  def erb_render
+    template = File.read('./lib/template.html.erb')
+    renderer = ERB.new(template)
+    result = renderer.result()
+    File.open('./lib/reunion_output.html', 'w') do |file|
+      file.write(result)
+    end
+  end
+end
