7cba5804825684b06209f70937f242635f0fbe71
Evan Wheeler <anon0mys87@gmail.com>
2018-02-21 09:20:24 -0700

    Activity class takes name and participants

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,9 @@
+# Does stuff
 class Activity
+  attr_reader :name, :participants
+
+  def initialize(name, participants)
+    @name = name
+    @participants = participants
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -3,5 +3,30 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
 
+# Tests for the Activity class
 class ActivityTest < Minitest::Test
+  def setup
+    @participants = { 'Tom' => 25.00, 'Jim' => 20.00 }
+    @activity = Activity.new('Dinner', @participants)
+  end
+
+  def test_it_initializes_with_a_name
+    assert_equal 'Dinner', @activity.name
+  end
+
+  def test_it_can_have_other_names
+    activity = Activity.new('Concert', 'Tom' => 30.00)
+
+    assert_equal 'Concert', activity.name
+  end
+
+  def test_it_has_participants
+    assert_equal @participants, @activity.participants
+  end
+
+  def test_participants_hash_tracks_amounts_paid
+    @activity.participants.each_value do |cost|
+      assert cost.is_a? Float
+    end
+  end
 end
